9b9bb21d66ff92b9302b6c228426e10748bfdd45
Matthew Montgomery <matthew.montgomery@planview.com>
2025-09-22 11:37:45 +1000

    Initial commit for challenges solution

---
+++ Challenges/Challenge1/IntFunctions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Challenge1
+{
+    public class IntFunctions
+    {
+        /// <summary>
+        /// Returns the minimum and maximum values found in the ineger array without using System.Linq
+        /// </summary>
+        /// <param name="inputArray">An input array of integers</param>
+        /// <returns>Returns a tuple of the min and max values found</returns>
+        public static (int min, int max) FindMinAndMax(int[] inputArray)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Challenges/Challenge1/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Challenge1;
+
+Console.WriteLine("complete the implementation of the findMinMax method");
+
+var inputArray = new int[] {1,6,8,7,34,235,5,678,97,4345,23,1,4};
+
+var response = IntFunctions.FindMinAndMax(inputArray);
+
+Console.WriteLine($"Min found to be : {response.min}");
+Console.WriteLine($"Max found to be : {response.max}");

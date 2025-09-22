53363d4b907552873d457f1e7d610528e0166b7f
Matthew Montgomery <matthew.montgomery@planview.com>
2025-09-22 11:43:41 +1000

    Added a FindTheAverage and FindTheMode methods to the Challenge1 project

--- Challenges/Challenge1/IntFunctions.cs
+++ Challenges/Challenge1/IntFunctions.cs
@@ -16,5 +16,25 @@ namespace Challenge1
         {
             throw new NotImplementedException();
         }
+
+        /// <summary>
+        /// Returns the average of the input array without using System.Linq
+        /// </summary>
+        /// <param name="inputArray">An input array of integers</param>
+        /// <returns>Returns the int value of the average</returns>
+        internal static object FindTheAverage(int[] inputArray)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Returns the mode (ie the most common entry) of the input array without using System.Linq
+        /// </summary>
+        /// <param name="inputArray">An input array of integers</param>
+        /// <returns>Returns the int value of the mode</returns>
+        internal static object FindTheMode(int[] inputArray)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Challenges/Challenge1/Program.cs
+++ Challenges/Challenge1/Program.cs
@@ -2,11 +2,19 @@
 
 using Challenge1;
 
-Console.WriteLine("complete the implementation of the findMinMax method");
+Console.WriteLine("complete the implementation of these methods");
 
-var inputArray = new int[] {1,6,8,7,34,235,5,678,97,4345,23,1,4};
+var inputArray = new int[] {1,6,8,7,34,1,2323,44,56,5,346,76,1,436346,4,87,5,342,5,235,5,678,97,4345,23,1,4,1};
 
 var response = IntFunctions.FindMinAndMax(inputArray);
 
 Console.WriteLine($"Min found to be : {response.min}");
 Console.WriteLine($"Max found to be : {response.max}");
+
+var average = IntFunctions.FindTheAverage(inputArray);
+
+Console.WriteLine($"Average found to be : {average}");
+
+var mode = IntFunctions.FindTheMode(inputArray);
+
+Console.WriteLine($"Mode found to be : {mode}");
